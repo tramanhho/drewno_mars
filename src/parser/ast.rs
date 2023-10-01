586263eca19755d804b06da75f0f980a458a1afc
Tram-Anh Ho <hotramanh19@gmail.com>
2023-09-30 19:46:48 -0500

    new ast wip

--- src/parser/ast.rs
+++ src/parser/ast.rs
@@ -1,182 +1,168 @@
-use crate::scanner::tokens::TokenType;
-
-//AST program
-#[derive(Clone, Debug, PartialEq)]
-pub enum Program {
-    Program(Box<Globals>),
-}
+// //AST program
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum Program {
+//     Program(Box<Globals>),
+// }
 
 
-//AST globals
-#[derive(Clone, Debug, PartialEq)]
-pub enum Globals {
-    Globals_Decl(Box<Globals>, Box<Decl>),
-    Globals_Epsilon(),
-}
+// //AST globals
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum Globals {
+//     Globals_Decl(Box<Globals>, Box<Decl>),
+//     Globals_Epsilon(),
+// }
 
 
-//AST decl
-#[derive(Clone, Debug, PartialEq)]
-pub enum Decl {
-    Decl_VarDecl(Box<VarDecl>, TokenType),
-    Decl_ClassDecl(Box<ClassDecl>),
-    Decl_FnDecl(Box<FnDecl>),
-}
+// //AST decl
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum Decl {
+//     Decl_VarDecl(Box<VarDecl>, TokenType),
+//     Decl_ClassDecl(Box<ClassDecl>),
+//     Decl_FnDecl(Box<FnDecl>),
+// }
 
-//AST varDecl
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum VarDecl {
-    VarDecl_Colon(Box<Id>, TokenType, Box<Type>),
-    VarDecl_ColonAssign(Box<Id>, TokenType, Box<Type>, TokenType, Box<Exp>),
+    Variable { var_type: Box<Type>, id: Box<Id> }
+    // Variable { var_type: Type, id: Id, init_val: Option<Box<Exp>> }
 }
 
 
-//AST type
 #[derive(Clone, Debug, PartialEq)]
 pub enum Type {
-    Type_PrimType(Box<PrimType>),
-    Type_PerfectPrimType(TokenType, Box<PrimType>),
-    Type_Id(Box<Id>),
-    Type_PerfectId(TokenType, Box<Id>),
+    Prim(PrimType),
+    Class(Box<Id>),
+    PerfectPrim(PrimType),
+    PerfectClass(String),
 }
 
-//AST primType
 #[derive(Clone, Debug, PartialEq)]
 pub enum PrimType {
-    PrimType_Int(TokenType),
-    PrimType_Bool(TokenType),
-    PrimType_Void(TokenType),
+    Bool,
+    Int,
+    Void,
 }
 
-//AST classDecl
-#[derive(Clone, Debug, PartialEq)]
-pub enum ClassDecl {
-    ClassDecl(Box<Id>, TokenType, TokenType, TokenType, Box<ClassBody>, TokenType, TokenType),
-}
+// //AST classDecl
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum ClassDecl {
+//     ClassDecl(Box<Id>, TokenType, TokenType, TokenType, Box<ClassBody>, TokenType, TokenType),
+// }
 
-//AST classBody
-#[derive(Clone, Debug, PartialEq)]
-pub enum ClassBody {
-    ClassBody_VarDecl(Box<ClassBody>, Box<VarDecl>, TokenType),
-    ClassBody_FnDecl(Box<ClassBody>, Box<FnDecl>, TokenType),
-    ClassBody_Epsilon(),
-}
+// //AST classBody
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum ClassBody {
+//     ClassBody_VarDecl(Box<ClassBody>, Box<VarDecl>, TokenType),
+//     ClassBody_FnDecl(Box<ClassBody>, Box<FnDecl>, TokenType),
+//     ClassBody_Epsilon(),
+// }
 
-//AST fnDecl
-#[derive(Clone, Debug, PartialEq)]
-pub enum FnDecl {
-    FnDecl_Formals(Box<Id>, TokenType, TokenType, Box<Formals>, TokenType, Box<Type>, TokenType, Box<StmtList>, TokenType),
-    //fnDecl(Box<Id>, TokenType, TokenType, TokenType, Box<Type>, TokenType, Box<StmtList>, TokenType),
-}
+// //AST fnDecl
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum FnDecl {
+//     FnDecl_Formals(Box<Id>, TokenType, TokenType, Box<Formals>, TokenType, Box<Type>, TokenType, Box<StmtList>, TokenType),
+//     //fnDecl(Box<Id>, TokenType, TokenType, TokenType, Box<Type>, TokenType, Box<StmtList>, TokenType),
+// }
 
-//AST formals
-#[derive(Clone, Debug, PartialEq)]
-pub enum Formals {
-    Formals_FormalsList(Box<FormalsList>),
-    Formals_Epsilon(),
-}
+// //AST formals
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum Formals {
+//     Formals_FormalsList(Box<FormalsList>),
+//     Formals_Epsilon(),
+// }
 
-//AST formalsList
-#[derive(Clone, Debug, PartialEq)]
-pub enum FormalsList {
-    FormalsList_FormalDecl(Box<FormalDecl>),
-    FormalsList(Box<FormalsList>, TokenType, Box<FormalDecl>),
-}
+// //AST formalsList
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum FormalsList {
+//     FormalsList_FormalDecl(Box<FormalDecl>),
+//     FormalsList(Box<FormalsList>, TokenType, Box<FormalDecl>),
+// }
 
-//AST formalDecl 
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum FormalDecl {
-    FormalDecl(Box<Id>, TokenType, Box<Type>),
+    FormalDecl(String, Box<Type>),
 }
 
-//AST stmtList
-#[derive(Clone, Debug, PartialEq)]
-pub enum StmtList {
-    StmtList_StmtList(Box<StmtList>, Box<Stmt>, TokenType),
-    StmtList_BlockStmt(Box<StmtList>, Box<BlockStmt>),
-    StmtList_Epsilon(),
-}
+// //AST stmtList
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum StmtList {
+//     StmtList_StmtList(Box<StmtList>, Box<Stmt>, TokenType),
+//     StmtList_BlockStmt(Box<StmtList>, Box<BlockStmt>),
+//     StmtList_Epsilon(),
+// }
 
 
-//AST blockStmt
-#[derive(Clone, Debug, PartialEq)]
-pub enum BlockStmt {
-    BlockStmt_While(TokenType, TokenType, Box<Exp>, TokenType, TokenType, Box<StmtList>, TokenType),
-    BlockStmt_If(TokenType, TokenType, Box<Exp>, TokenType, TokenType, Box<StmtList>, TokenType),
-    BlockStmt_IfElse(TokenType, TokenType, Box<Exp>, TokenType, TokenType, Box<StmtList>, TokenType, TokenType, TokenType, Box<StmtList>, TokenType),
-}
-//AST stmt
-#[derive(Clone, Debug, PartialEq)]
-pub enum Stmt {
-    Stmt_VarDecl(Box<VarDecl>),
-    Stmt_Assign(Box<Loc>, TokenType, Box<Exp>),
-    Stmt_PostDec(Box<Loc>, TokenType),
-    Stmt_PostInc(Box<Loc>, TokenType),
-    Stmt_Give(TokenType, Box<Exp>),
-    Stmt_Take(TokenType, Box<Loc>),
-    Stmt_ReturnExp(TokenType, Box<Exp>),
-    Stmt_Return(TokenType),
-    Stmt_Exit(TokenType),
-    Stmt_CallExp(Box<CallExp>),
-}
+// //AST blockStmt
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum BlockStmt {
+//     BlockStmt_While(TokenType, TokenType, Box<Exp>, TokenType, TokenType, Box<StmtList>, TokenType),
+//     BlockStmt_If(TokenType, TokenType, Box<Exp>, TokenType, TokenType, Box<StmtList>, TokenType),
+//     BlockStmt_IfElse(TokenType, TokenType, Box<Exp>, TokenType, TokenType, Box<StmtList>, TokenType, TokenType, TokenType, Box<StmtList>, TokenType),
+// }
+// //AST stmt
+// #[derive(Clone, Debug, PartialEq)]
+// pub enum Stmt {
+//     Stmt_VarDecl(Box<VarDecl>),
+//     Stmt_Assign(Box<Loc>, TokenType, Box<Exp>),
+//     Stmt_PostDec(Box<Loc>, TokenType),
+//     Stmt_PostInc(Box<Loc>, TokenType),
+//     Stmt_Give(TokenType, Box<Exp>),
+//     Stmt_Take(TokenType, Box<Loc>),
+//     Stmt_ReturnExp(TokenType, Box<Exp>),
+//     Stmt_Return(TokenType),
+//     Stmt_Exit(TokenType),
+//     Stmt_CallExp(Box<CallExp>),
+// }
 
-//AST exp
 #[derive(Clone, Debug, PartialEq)]
 pub enum Exp {
-    Exp_Dash(Box<Term>, TokenType, Box<Exp>),
-    Exp_Cross(Box<Term>, TokenType, Box<Exp>),
-    Exp_Star(Box<Term>, TokenType, Box<Exp>),
-    Exp_Slash(Box<Term>, TokenType, Box<Exp>),
-    Exp_And(Box<Term>, TokenType, Box<Exp>),
-    Exp_Or(Box<Term>, TokenType, Box<Exp>),
-    Exp_Equals(Box<Term>, TokenType, Box<Exp>),
-    Exp_NotEquals(Box<Term>, TokenType, Box<Exp>),
-    Exp_Greater(Box<Term>, TokenType, Box<Exp>),
-    Exp_GreaterEq(Box<Term>, TokenType, Box<Exp>),
-    Exp_Less(Box<Term>, TokenType, Box<Exp>),
-    Exp_LessEq(Box<Term>, TokenType, Box<Exp>),
-    Exp_Not(TokenType, Box<Exp>),
-    Exp_DashTerm(TokenType, Box<Term>),
-    Exp_Term(Box<Term>),
+    True,
+    False,
+    Magic,
+    UnaryExp(Box<UnaryExp>),
+    BinaryExp(Box<BinaryExp>),
+    CallExp(Box<CallExp>),
+    IntLit(i32),
+    StrLit(String),
+    Loc(Box<Loc>),
 }
 
-//AST callExp
+#[derive(Clone, Debug, PartialEq)]
+pub enum UnaryExp {
+    Neg { exp: Box<Exp>},
+    Not { exp: Box<Exp>},
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum BinaryExp {
+    And       { lhs: Box<Exp>, rhs: Box<Exp>},
+    Or        { lhs: Box<Exp>, rhs: Box<Exp>},
+    Equals    { lhs: Box<Exp>, rhs: Box<Exp>},
+    NotEquals { lhs: Box<Exp>, rhs: Box<Exp>},
+    Greater   { lhs: Box<Exp>, rhs: Box<Exp>},
+    Less      { lhs: Box<Exp>, rhs: Box<Exp>},
+    GreaterEq { lhs: Box<Exp>, rhs: Box<Exp>},
+    LessEq    { lhs: Box<Exp>, rhs: Box<Exp>},
+    Plus      { lhs: Box<Exp>, rhs: Box<Exp>},
+    Minus     { lhs: Box<Exp>, rhs: Box<Exp>},
+    Times     { lhs: Box<Exp>, rhs: Box<Exp>},
+    Divide    { lhs: Box<Exp>, rhs: Box<Exp>},
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum CallExp {
-    CallExp_Fn(Box<Id>, TokenType, TokenType),
-    CallExp_FnArgs(Box<Id>, TokenType, Box<ActualsList>, TokenType),
+    Fn {name: Box<Loc>, args: Vec<Box<Exp>>}
 }
 
-//AST actualsList
-#[derive(Clone, Debug, PartialEq)]
-pub enum ActualsList {
-    ActualsList_Exp(Box<Exp>),
-    ActualsList_ActualsList(Box<ActualsList>, TokenType, Box<Exp>),
-}
-
-//AST term
-#[derive(Clone, Debug, PartialEq)]
-pub enum Term {
-    Term_Loc(Box<Loc>),
-    Term_IntLiteral(i32),
-    Term_StringLiteral(String),
-    Term_True(TokenType),
-    Term_False(TokenType),
-    Term_Magic(TokenType),
-    Term_Paren(TokenType, Box<Exp>, TokenType),
-    Term_CallExp(Box<CallExp>),
-}
-
-//AST loc
 #[derive(Clone, Debug, PartialEq)]
 pub enum Loc {
-    Loc_Id(Box<Id>),
-    Loc_PostDec(Box<Loc>, TokenType, Box<Id>),
+    Id(Box<Id>),
+    MemberFieldExp { base_class: Box<Loc>, field_name: String }
 }
 
-
-//AST id
 #[derive(Clone, Debug, PartialEq)]
 pub enum Id {
-    Id(String),
+    Name(String)
 }
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -12,124 +12,264 @@ mod tests {
     use super::lexer::Lexer;
     use super::grammar::*;
 
-    #[test]
-    fn parse_program() {
-        let program_good = [
-            "a : bool = ! true;",
-            "a : bool = !!too hot;",
-            "myClass:class {
-                a:int = 12;
-                b:int;
-                c:perfect bool;
-            };",
-            "myClass:class {};",
-        ];
+    pub enum ParserType {
+        Type,
+        PrimType,
 
-        let program_bad = [
-            "main();",
-            "myClass:class {
-                a:int = 12
-                b:int
-                c:perfect bool
-            };",
-        ];
+        FormalList,
+        FormalDecl,
+        
+        // Exp,
+        // ActualsList,
+        CallExp,
+        Exp,
+        Term,
+        Loc,
+        Id,
+    }
 
-        for prog in program_good.iter() {
-            // println!("{:?}\n", ProgramParser::new().parse(Lexer::new(prog)).unwrap());
-            assert!(
-                ProgramParser::new().parse(Lexer::new(prog)).is_ok(),
-                "\nThe following input did not pass the parser as intended:\n{}\n", prog, 
-            ); 
-        }
+    pub enum InputType {
+        Good,
+        Bad,
+    }
 
-        for prog in program_bad.iter() {
-            assert!(
-                ProgramParser::new().parse(Lexer::new(prog)).is_err(),
-                "\nThe following input did not fail the parser as intended:\n{:?}\n", prog, 
-            );
+    fn test_inputs(good_tests: Vec<&'static str>, bad_tests: Option<Vec<&'static str>>, parse: &ParserType) {
+        test_inputs_helper(good_tests, parse, &InputType::Good);
+        match bad_tests {
+            Some(t) => test_inputs_helper(t, parse, &InputType::Bad),
+            None => ()
         }
     }
 
+    fn test_inputs_helper(tests: Vec<&'static str>, parse: &ParserType, input: &InputType) {
+        for t in tests.iter() {
+            let mut result = match parse {
+                ParserType::Type  => TypeParser::new() .parse(Lexer::new(t)).is_ok(),
+                ParserType::PrimType  => PrimTypeParser::new() .parse(Lexer::new(t)).is_ok(),
+
+                ParserType::FormalList  => FormalsListParser::new() .parse(Lexer::new(t)).is_ok(),
+                ParserType::FormalDecl  => FormalDeclParser::new() .parse(Lexer::new(t)).is_ok(),
+                
+                // ParserType::ActualsList  => ActualsListParser::new() .parse(Lexer::new(t)).is_ok(),
+                ParserType::CallExp  => CallExpParser::new() .parse(Lexer::new(t)).is_ok(),
+                ParserType::Exp  => ExpParser::new() .parse(Lexer::new(t)).is_ok(),
+                ParserType::Term => TermParser::new().parse(Lexer::new(t)).is_ok(),
+                ParserType::Loc  => LocParser::new() .parse(Lexer::new(t)).is_ok(),
+                ParserType::Id   => IdParser::new()  .parse(Lexer::new(t)).is_ok(),
+            };
+            
+            let err_string : String;
+            match input {
+                InputType::Good => {
+                    err_string = format!("\nThe following input did not pass the parser as intended:\n{}\n", t)
+                },
+                InputType::Bad => {
+                    result = !result;
+                    err_string = format!("\nThe following input did not fail the parser as intended:\n{}\n", t)
+                }
+            };
+
+            assert!(result, "{err_string}");
+        }
+    }
+
+    // #[test]
+    // fn parse_program() {
+    //     let program_good = [
+    //         "a : bool = ! true;",
+    //         "a : bool = !!too hot;",
+    //         "myClass:class {
+    //             a:int = 12;
+    //             b:int;
+    //             c:perfect bool;
+    //         };",
+    //         "myClass:class {};",
+    //     ];
+
+    //     let program_bad = [
+    //         "main();",
+    //         "myClass:class {
+    //             a:int = 12
+    //             b:int
+    //             c:perfect bool
+    //         };",
+    //     ];
+    //     for prog in program_good.iter() {
+    //         // println!("{:?}\n", ProgramParser::new().parse(Lexer::new(prog)).unwrap());
+    //         assert!(
+    //             ProgramParser::new().parse(Lexer::new(prog)).is_ok(),
+    //             "\nThe following input did not pass the parser as intended:\n{}\n", prog, 
+    //         ); 
+    //     }
+
+    //     for prog in program_bad.iter() {
+    //         assert!(
+    //             ProgramParser::new().parse(Lexer::new(prog)).is_err(),
+    //             "\nThe following input did not fail the parser as intended:\n{:?}\n", prog, 
+    //         );
+    //     }
+    // }
+
     #[test]
-    fn parse_stmt() {
-        let stmt_good = [
-            "a--a = magic",
-            "a--b--c--d--e = magic",
-            "a--",
-            "a++",
-            "give magic",
-            "take a--b",
-            "take meow_on",
-            "return true",
-            "return",
-            "today I don't feel like doing any work",
-            "abc()",
-            "owo(uwu)",
+    fn parse_type() {
+        let type_good = [
+            "int",
+            "bool",
+            "void",
+            "perfect int",
+            "perfect owo",
+            "perfect _nya",
         ];
 
-        let stmt_bad = [
-            "take a-b",
-            "abc--abc a--c",
+        let type_bad = [
+            "perfect",
+            "perfect 123",
         ];
         
-        for stmt in stmt_good.iter() {
+        for t in type_good.iter() {
             assert!(
-                StmtParser::new().parse(Lexer::new(stmt)).is_ok(),
-                "\nThe following input did not pass the parser as intended:\n{}\n", stmt, 
+                TypeParser::new().parse(Lexer::new(t)).is_ok(),
+                "\nThe following input did not pass the parser as intended:\n{}\n", t, 
             );
+            println!("{:?}", TypeParser::new().parse(Lexer::new(t)).unwrap());
         }
 
-        for stmt in stmt_bad.iter() {
-            assert!(
-                StmtParser::new().parse(Lexer::new(stmt)).is_err(),
-                "\nThe following input did not fail the parser as intended:\n{:?}\n", stmt, 
-            );
+        for t in type_bad.iter() {
+            assert!(TypeParser::new().parse(Lexer::new(t)).is_err());
         }
     }
 
     #[test]
-    fn parse_exp() {
-        let exp_good = [
-            r#"not (123 + "abc")"#,
-            "-nice",
-            "!!too hot",
-            "abc - abc - abc",
-            "abc + abc",
-            "abc * abc",
-            "abc / abc",
-            "abc and abc",
-            "abc or abc",
-            "abc == abc",
-            "abc != abc",
-            "abc > abc",
-            "abc >= abc",
-            "abc < abc",
-            "abc <= abc",
-            "abc",
+    fn parse_prim_type() {
+        let prim_type = [
+            "int",
+            "bool",
+            "void",
         ];
-
-        let exp_bad = [
-            "abc abc",
-        ];
-
-        for exp in exp_good.iter() {
-            assert!(ExpParser::new().parse(Lexer::new(exp)).is_ok());    
-        }
-
-        for exp in exp_bad.iter() {
-            assert!(ExpParser::new().parse(Lexer::new(exp)).is_err());
+        
+        for pt in prim_type.iter() {
+            assert!(
+                PrimTypeParser::new().parse(Lexer::new(pt)).is_ok(),
+                "\nThe following input did not pass the parser as intended:\n{}\n", pt, 
+            );
+            println!("{:?}", PrimTypeParser::new().parse(Lexer::new(pt)).unwrap());
         }
     }
+
+    
+    #[test]
+    fn parse_formal_list() {
+        let fd_good = [
+            "a: int, b: void, c: owo",
+            "_owo : int",
+            "",
+        ];
+        
+        for fd in fd_good.iter() {
+            assert!(
+                FormalsListParser::new().parse(Lexer::new(fd)).is_ok(),
+                "\nThe following input did not pass the parser as intended:\n{}\n", fd, 
+            );
+            println!("{:?}", FormalsListParser::new().parse(Lexer::new(fd)).unwrap());
+        }
+    }
+
+    #[test]
+    fn parse_formal_decl() {
+        let fd_good = [
+            "meow : perfect _nya",
+            "_owo : int",
+        ];
+        
+        for fd in fd_good.iter() {
+            assert!(
+                FormalDeclParser::new().parse(Lexer::new(fd)).is_ok(),
+                "\nThe following input did not pass the parser as intended:\n{}\n", fd, 
+            );
+            println!("{:?}", FormalDeclParser::new().parse(Lexer::new(fd)).unwrap());
+        }
+    }
+
+    // #[test]
+    // fn parse_stmt() {
+    //     let stmt_good = [
+    //         "a--a = magic",
+    //         "a--b--c--d--e = magic",
+    //         "a--",
+    //         "a++",
+    //         "give magic",
+    //         "take a--b",
+    //         "take meow_on",
+    //         "return true",
+    //         "return",
+    //         "today I don't feel like doing any work",
+    //         "abc()",
+    //         "owo(uwu)",
+    //     ];
+
+    //     let stmt_bad = [
+    //         "take a-b",
+    //         "abc--abc a--c",
+    //     ];
+        
+    //     for stmt in stmt_good.iter() {
+    //         assert!(
+    //             StmtParser::new().parse(Lexer::new(stmt)).is_ok(),
+    //             "\nThe following input did not pass the parser as intended:\n{}\n", stmt, 
+    //         );
+    //     }
+
+        // for stmt in stmt_bad.iter() {
+        //     assert!(
+        //         StmtParser::new().parse(Lexer::new(stmt)).is_err(),
+        //         "\nThe following input did not fail the parser as intended:\n{:?}\n", stmt, 
+        //     );
+        // }
+    // }
+
+    // #[test]
+    // fn parse_exp() {
+    //     let exp_good = [
+    //         r#"not (123 + "abc")"#,
+    //         "-nice",
+    //         "!!too hot",
+    //         "abc - abc - abc",
+    //         "abc + abc",
+    //         "abc * abc",
+    //         "abc / abc",
+    //         "abc and abc",
+    //         "abc or abc",
+    //         "abc == abc",
+    //         "abc != abc",
+    //         "abc > abc",
+    //         "abc >= abc",
+    //         "abc < abc",
+    //         "abc <= abc",
+    //         "abc",
+    //     ];
+
+    //     let exp_bad = [
+    //         "abc abc",
+    //     ];
+
+    //     for exp in exp_good.iter() {
+    //         assert!(ExpParser::new().parse(Lexer::new(exp)).is_ok());    
+    //     }
+
+    //     for exp in exp_bad.iter() {
+    //         assert!(ExpParser::new().parse(Lexer::new(exp)).is_err());
+    //     }
+    // }
 
     #[test]
     fn parse_call_exp() {
-        let call_exp_good = [
+        let call_exp_good = vec![
             "___ (a, b, c)",
             "_ (true, false, too hot)",
             "a (a/3+a)",
         ];
 
-        let call_exp_bad = [
+        let call_exp_bad = vec![
             "a (",
             "a )",
             "a (",
@@ -137,41 +277,29 @@ mod tests {
             "abc",
         ];
 
-        for call_exp in call_exp_good.iter() {
-            assert!(CallExpParser::new().parse(Lexer::new(call_exp)).is_ok());    
-        }
-
-        for call_exp in call_exp_bad.iter() {
-            assert!(CallExpParser::new().parse(Lexer::new(call_exp)).is_err());
-        }
+        test_inputs(call_exp_good, Some(call_exp_bad), &ParserType::CallExp);
     }
 
     #[test]
     fn parse_actuals_list() {
-        let actuals_lists_good = [
+        let actuals_lists_good = vec![
             "a--b--c--d",
             "true, true, true, true, true",
             r#"true, "pleading", 54353, magic, (too hot)"#,
             "a+b, too hot",
         ];
 
-        let actuals_lists_bad = [
+        let actuals_lists_bad = vec![
             ",",
             "true, ",
         ];
 
-        for actual_list in actuals_lists_good.iter() {
-            assert!(ActualsListParser::new().parse(Lexer::new(actual_list)).is_ok());    
-        }
-
-        for actual_list in actuals_lists_bad.iter() {
-            assert!(ActualsListParser::new().parse(Lexer::new(actual_list)).is_err());
-        }
+        // test_inputs(actuals_lists_good, Some(actuals_lists_bad), &ParserType::ActualsList);
     }
 
     #[test]
     fn parse_term() {
-        let terms_good = [
+        let terms_good = vec![
             "a--b--c--d",
             "5273985",
             r#""meow meow meow \t \n""#,
@@ -183,53 +311,38 @@ mod tests {
             "a (a-b)",
             "(!!too hot)",
             "(a-b)",
+            "a-b-c-d-e-f",
         ];
 
-        let terms_bad = [
+        let terms_bad = vec![
             "2147483648",
             r#""meow meow meow"#,
             r#""heewoo!??! \g""#,
             r#""\g"#,
         ];
-
-        for term in terms_good.iter() {
-            assert!(
-                TermParser::new().parse(Lexer::new(term)).is_ok(),
-                "\nThe following input did not pass the parser as intended:\n{}\n", term, 
-            );
-            // println!("{:?}", TermParser::new().parse(Lexer::new(term)).unwrap());
-        }
-
-        for term in terms_bad.iter() {
-            println!("{:?}", TermParser::new().parse(Lexer::new(term)));
-        }
+        
+        test_inputs(terms_good, Some(terms_bad), &ParserType::Term);
     }
 
     #[test]
     fn parse_loc() {
-        let locs_good = [
+        let locs_good = vec![
             "a--b--c--d",
             "owo--owo",
             "_nya_",
         ];
 
-        let locs_bad = [
+        let locs_bad = vec![
             "a--b--c--d--",
             "--",
         ];
         
-        for loc in locs_good.iter() {
-            assert!(LocParser::new().parse(Lexer::new(loc)).is_ok());
-        }
-
-        for loc in locs_bad.iter() {
-            assert!(LocParser::new().parse(Lexer::new(loc)).is_err());
-        }
+        test_inputs(locs_good, Some(locs_bad),&ParserType::Loc);
     }
 
     #[test]
     fn parse_id() {
-        let ids_good = [
+        let ids_good = vec![
             "a",
             "abc",
             "fsndjkfngjkreng",
@@ -238,20 +351,12 @@ mod tests {
             "ffds215345",
         ];
 
-        let ids_bad = [
+        let ids_bad = vec![
             "0_abc",
             "",
             ":pleading:",
         ];
-
-        for id in ids_good.iter() {
-            assert!(IdParser::new().parse(Lexer::new(id)).is_ok());
-            println!("{:?}", IdParser::new().parse(Lexer::new(id)).unwrap());
-        }
-
-        for id in ids_bad.iter() {
-            assert!(IdParser::new().parse(Lexer::new(id)).is_err());
-        }
+        test_inputs(ids_good, Some(ids_bad), &ParserType::Id);
     }
     
 }
